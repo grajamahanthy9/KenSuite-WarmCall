7a397fbb0d1a9d37822ed40444615e740fb37ec7
Ganesh Rajamahanthy <kensuite99@gmail.com>
2018-02-05 00:58:42 +0530

    First Commit

---
+++ WarmCallApp/Controllers/AppController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WarmCallApp.Controllers
+{
+    /// <summary>
+    /// Create an ActionResult and PartialView for each angular partial view you want to attatch to a route in the angular app.js file.
+    /// </summary>
+    public class AppController : Controller
+    {
+        public ActionResult Register(int IsBuyer)
+        {
+            ViewBag.IsBuyer = IsBuyer;
+            return PartialView("Register");
+        }
+        public ActionResult SignIn(int IsBuyer)
+        {
+            ViewBag.IsBuyer = IsBuyer;
+            return PartialView("SignIn");
+        }
+        public ActionResult Home()
+        {
+            return PartialView();
+        }
+
+        [Authorize]
+        public ActionResult TodoManager()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ WarmCallApp/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WarmCallApp.WebApiApplication" Language="C#" %>
